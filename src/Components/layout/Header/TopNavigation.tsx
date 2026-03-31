614c0c47d89575213e1e3b9d7c7f05707a1f3a20
kacper-echaust <k9echaust@gmail.com>
2026-04-01 01:55:43 +0200

    short code and add new button

--- src/Components/layout/Header/TopNavigation.tsx
+++ src/Components/layout/Header/TopNavigation.tsx
@@ -1,21 +1,30 @@
+import ButtonLink from "../../shared/ButtonLink";
+
+const data = ["O badaniu", "Wyniki", "Ambasadorzy", "Dołącz do inicjatywy!"];
+
 export function TopNavigation() {
   return (
     <nav>
       <ul className="flex items-center">
-        <li className="px-2 py-3 font-bold whitespace-nowrap hover:underline flex items-center">
-          <a href="#TODO">O badaniu</a>
-          <span className="ml-2 h-[1em] border-r border-black"></span>
-        </li>
-        <li className="px-2 py-3 font-bold whitespace-nowrap hover:underline flex items-center">
-          <a href="#TODO">Wyniki</a>
-          <span className="ml-2 h-[1em] border-r border-black"></span>
-        </li>
-        <li className="px-2 py-3 font-bold whitespace-nowrap hover:underline flex items-center">
-          <a href="#TODO">Ambasadorzy</a>
-          <span className="ml-2 h-[1em] border-r border-black"></span>
-        </li>
-        <li className="px-2 py-3 font-bold whitespace-nowrap hover:underline">
-          <a href="#TODO">Chcę dołączyć!</a>
+        {data.map((item, index) => (
+          <li
+            key={index}
+            className="px-2 py-3 font-bold whitespace-nowrap hover:underline flex items-center"
+          >
+            <a href="#">{item}</a>
+            <span className="ml-2 h-[2em] border-r-2 border-black"></span>
+          </li>
+        ))}
+
+        <li>
+          <ButtonLink
+            className="rounded-2xl border-black border px-6 font-thin"
+            color="green"
+            variant="solid"
+            href="#"
+          >
+            Wesprzyj nas
+          </ButtonLink>
         </li>
       </ul>
     </nav>
