fc6c22d54b0015a239d9056a7f25733672cb740e
kacper-echaust <k9echaust@gmail.com>
2026-04-01 05:39:28 +0200

    fix backgrounds

--- src/Components/StartSection.tsx
+++ src/Components/StartSection.tsx
@@ -2,8 +2,14 @@ import ButtonLink from "./shared/ButtonLink";
 import woman from "../assets/img/woman.png";
 const StartSection = () => {
   return (
-    <section className="h-[75vh] bg-white relative before:absolute  before:h-full before:w-full overflow-hidden before:bg-[url('/backgrounds/straight-lines.svg')] before:bg-cover before:bg-no-repeat before:opacity-30 grid grid-cols-2">
-      <div className=" flex flex-col  justify-center  h-full px-12">
+    <section className="grid grid-cols-1 md:grid-cols-2 mt-24 pb-24 relative">
+      <div
+        className="opacity-30 bg-cover bg-no-repeat absolute inset-0"
+        style={{
+          backgroundImage: "url('/backgrounds/straight-lines.svg')",
+        }}
+      ></div>
+      <div className=" flex flex-col  justify-center  h-full px-12 gap-4 w-full">
         <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
           <span className="text-brandGraphite">Masz wpływ</span>
           <br />
@@ -11,11 +17,14 @@ const StartSection = () => {
           <span className="text-brandBlue">szczęście</span>
         </h2>
 
-        <p className="text-xl md:text-2xl font-bold mb-8">
+        <p
+          className="text-xl md:text-2xl font-bold mb-
+        8"
+        >
           <span className="text-brandGraphite">Zrób to </span>
           <span className="text-brandBlue">#MałymiKrokami.</span>
         </p>
-        <p className="font-thin text-sm text-center  mb-4 px-56">
+        <p className="font-thin text-sm mb-4 md:text-xl">
           Chcemy pokazać, że zmiany na lepsze nie oznaczają wywracania życia do
           góry nogami. Wystarczy{" "}
           <span className="font-bold">systematyka i 10 minut dziennie!</span>
@@ -29,8 +38,8 @@ const StartSection = () => {
           Dołącz do inicjatywy!
         </ButtonLink>
       </div>
-      <div className="w-full h-full flex items-center justify-center">
-        <img src={woman} alt="" className="object-cover w-3/4" />
+      <div className="w-96 flex items-center justify-center">
+        <img src={woman} alt="" className="object-cover w-full" />
       </div>
     </section>
   );
--- src/Components/layout/Footer/Footer.tsx
+++ src/Components/layout/Footer/Footer.tsx
@@ -5,7 +5,12 @@ import { TikTok } from "../../../assets/icons/TikTok";
 
 export default function Footer() {
   return (
-    <footer className="relative bg-[url('/backgrounds/dots.svg')] bg-cover bg-repeat py-20 px-4">
+    <footer
+      className="relative bg-brandSecondWhite bg-cover bg-repeat py-20 px-4"
+      style={{
+        backgroundImage: "url('/backgrounds/dots.png')",
+      }}
+    >
       <div className="absolute inset-0 bg-white/85"></div>
       <div className="container mx-auto relative z-10 px-[25px]">
         {/* Layout responsywny - kolumna na mobile, wiersz na desktop */}
